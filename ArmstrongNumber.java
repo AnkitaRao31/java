e9d6272ac5f414af7eb6d4fb7c1424060879fceb
Ankita Rao <raoankita10@gmail.com>
2024-11-11 11:33:44 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -2,28 +2,27 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+        int remainder, result = 0;
         
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
         
         int originalNumber = number;
-        int sum = 0;
         
-        int numberOfDigits = String.valueOf(number).length();
-        
-       
-        while (number > 0) {
-            int digit = number % 10;
-            sum += Math.pow(digit, numberOfDigits); 
-            number /= 10;
+        while (originalNumber != 0) 
+        {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
         }
         
   
-        if (sum == originalNumber) {
-            System.out.println(originalNumber + " is an Armstrong number.");
+        if (result == number) {
+            System.out.println(number + " is an Armstrong number.");
         } else {
-            System.out.println(originalNumber + " is not an Armstrong number.");
+            System.out.println(number + " is not an Armstrong number.");
         }
         
         sc.close();
